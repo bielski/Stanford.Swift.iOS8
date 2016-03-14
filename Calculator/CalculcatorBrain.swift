c4c4e427fd2f7edbd35129b0b002522c35145c50
Ewa Bielska <ewa.bielska@allegro.pl>
2016-03-14 14:43:32 +0100

    Initial commit

---
+++ Calculator/CalculcatorBrain.swift
@@ -0,0 +1,95 @@
+//
+//  CalculcatorBrain.swift
+//  Calculator
+//
+//  Created by Ewa Bielska on 14/03/16.
+//  Copyright © 2016 Allegro Group. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain {
+
+    private enum Op: CustomStringConvertible {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+
+        var description: String {
+            get {
+                switch self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .UnaryOperation(let symbol, _):
+                    return symbol
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                }
+            }
+        }
+    }
+
+    private var opStack = [Op]()
+    private var knownOps = [String:Op]()
+
+    init() {
+
+        func learnOp(op: Op) {
+            knownOps[op.description] = op
+        }
+
+        learnOp(Op.BinaryOperation("✕", *))
+        learnOp(Op.BinaryOperation("+", +))
+        learnOp(Op.BinaryOperation("-", -))
+        learnOp(Op.BinaryOperation("÷", /))
+        learnOp(Op.UnaryOperation("√", sqrt))
+    }
+
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let operand1Evaluation = evaluate(remainingOps)
+                if let operand1 = operand1Evaluation.result {
+                    let operand2Evaluation = evaluate(operand1Evaluation.remainingOps)
+                    if let operand2 = operand2Evaluation.result {
+                        return (operation(operand1, operand2), operand2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        return (nil, ops)
+    }
+
+    func evaluate() -> Double? {
+        let (result, remainder) = evaluate(opStack)
+        print("\(opStack) = \(result) with \(remainder) left over")
+        return result
+    }
+
+    func pushOperand(operand: Double) -> Double? {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+
+    func performOperation(symbol: String) -> Double? {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+        return evaluate()
+    }
+
+    func clearOpStack() {
+        opStack.removeAll()
+    }
+}
---
+++ Calculator/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Ewa Bielska on 11/03/16.
+//  Copyright © 2016 Allegro Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    var userIsInTheMiddleOfTyping = false
+    var brain = CalculatorBrain()
+
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+
+        if userIsInTheMiddleOfTyping {
+            if digit == "." && (display.text!.rangeOfString(digit) != nil) {
+                return
+            }
+            display.text = display.text! + digit
+
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+
+    @IBAction func appendConstant(sender: AnyObject) {
+        display.text = "\(M_PI)"
+        enter()
+    }
+
+    @IBAction func enter() {
+        userIsInTheMiddleOfTyping = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+    }
+
+    @IBAction func clear(sender: AnyObject) {
+        brain.clearOpStack()
+        displayValue = 0
+    }
+
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTyping = false
+        }
+    }
+
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            enter()
+        }
+
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+    }
+}
